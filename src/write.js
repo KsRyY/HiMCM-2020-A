ea2747086dd9f2cf6705590c3c4fd308d1e92ca3
unknown <25174488+KsRyY@users.noreply.github.com>
2020-11-15 15:38:25 +0800

    automatically create nonexisting directory before write

--- src/write.js
+++ src/write.js
@@ -6,6 +6,7 @@ import { formatOutput } from './format.js'
 export async function write(outdir, filename, ...args) {
 	const generatedData = generateData(...args)
 	const formattedData = formatOutput(generatedData)
+	await fs.mkdirp(path.resolve(outdir))
 	await fs.writeFile(path.resolve(outdir, `${filename}.txt`), formattedData)
 	await fs.writeFile(
 		path.resolve(outdir, `${filename}.json`),
